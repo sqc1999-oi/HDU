acf4640f3d08d525579476266ab26ea1aaecbb6f
孙乾程 <sqc1999@outlook.com>
2016-01-02 10:27:22 +0800

    u

--- ChangeEncoding.ps1
+++ ChangeEncoding.ps1
@@ -1,7 +1,7 @@
 $Encoding = New-Object System.Text.UTF8Encoding($False)
 foreach ($f in (dir -Recurse))
 {
-    if ($f.Extension -eq ".sln" -or $f.Extension -eq ".cs" -or $f.Extension -eq ".csproj" -or $f.Extension -eq ".vcxproj" -or $f.Extension -eq ".cpp")
+    if ($f.Extension -eq ".cs" -or $f.Extension -eq ".csproj" -or $f.Extension -eq ".vcxproj" -or $f.Extension -eq ".cpp")
     {
         $s = [System.IO.File]::ReadAllLines($f.FullName)
         [System.IO.File]::WriteAllLines($f.FullName, $s, $Encoding)
